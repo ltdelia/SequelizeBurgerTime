679157bf0046184b3b58b72815961503061b7ef8
ltdelia <delia.louis@gmail.com>
2016-06-21 14:26:32 -0400

    updated logic to display devoured burgers under Devoured panel

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,6 +3,9 @@ var Burger = require('../model/burger.js');
 // Routing
 
 module.exports = function(app){
+
+	// Our home page. The app will get the path '/'. 
+	// Sequelize will "SELECT * " from our table, then render the result in index.handlebars
 	app.get('/', function(req, res){
 		Burger.findAll({})
 			.then(function(result){
@@ -10,6 +13,8 @@ module.exports = function(app){
 			})
 	});	
 	
+	// Our API. The app will get the path '/api'.
+	// Sequelie will "SELECT * " from our table, then respond with a JSON of our data
 	app.get('/api', function(req, res){
 		Burger.findAll({})
 			.then(function(result){
@@ -17,11 +22,14 @@ module.exports = function(app){
 			})
 	});
 
+	// When posting to the API, the app will receive a request
 	app.post('/api', function(req, res){
+		// This request will take the form of the object burger
+		// When a user clicks "Order this burger!", we'll send the burger_name
+		// When a user clicks "Devour It!", we'll send the id
 		var burger = req.body;
-
-	// How to check to see if burger already exists?
-	
+		
+		// We create a new burger when the burger_name is provided			
 		Burger.create({
 			id: burger.id,
 			burger_name: burger.burger_name,
@@ -29,14 +37,30 @@ module.exports = function(app){
 			date: burger.date
 		});
 
-		// Burger.update({
-		// 	devoured: true,
-		// },{ 
-		// 	where: {
-		// 		burger_name: burger
-		// 	}
-		// });
-
+		// In order to update the table, we need to first "SELECT * "
+		Burger.findAll({})
+			.then(function(burgers){
+				// ...loop through the array of burger objects...
+				for(var i=0; i<burgers.length; i++){
+					console.log("-------------------");
+					console.log(burgers[i].id);
+					console.log(burgers[i].burger_name);
+					console.log(burgers[i].devoured);
+					console.log(burgers[i].date);
+					console.log("-------------------");
+					// ...and if the id sent matches an id in the table...
+					if(burger.id == burgers[i].id){
+						// ...run an UPDATE to the table, setting devoured=true where the ids match
+						Burger.update({
+							devoured: true,
+						},{ 
+							where: {
+								id: burger.id
+							}
+						});
+					}
+				}
+			})
 	});
 
 }
